c64e5148887bcdd911bf6f79a9367b5b582ce09f
gianlucafalaschi <gian100gian@outlook.it>
2024-05-14 16:43:10 +0200

    print movies

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ public function __construct($_title, $_genre) {
 
 // Funzione che da il nome e l'anno
 public function getMovieTitleAndYear() {
-    return 'Titolo del film:' . ' ' . $this->title . ' ' . 'Anno del film:' . ' ' . $this->year; 
+    return 'Titolo del film:' . ' ' .  $this->title . '<br> ' . 'Anno del film:' . ' ' . $this->year; 
 }
 
 }
@@ -27,8 +27,8 @@ $indipendeceDay->director = 'Roland Emmerich';
 /* $indipendeceDay->genre = 'Fantascienza'; */
 $indipendeceDay->year = '1996';
 
-var_dump($indipendeceDay);
-echo $indipendeceDay->getMovieTitleAndYear();
+/* var_dump($indipendeceDay); */
+/* echo $indipendeceDay->getMovieTitleAndYear(); */
 
 $interstellar = new Movie('Interstellar', 'Fantascienza');
 /* $interstellar->title = 'Interstellar'; */
@@ -37,6 +37,32 @@ $interstellar->director = 'Christopher Nolan';
 $interstellar->year = '2014';
 
 
-var_dump($interstellar); 
+/* var_dump($interstellar);  */
+/* echo $interstellar->getMovieTitleAndYear(); */
+
+
+$movies = [
+    $indipendeceDay,
+    $interstellar
+];
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach($movies as $movie) { ?>
+        <div>
+            <div><?php echo $movie->getMovieTitleAndYear(); ?></div>
+            <div>Director: <?php echo $movie->director ?></div>
+        </div><br>
+    <?php } ?>
+    
+</body>
+</html>
