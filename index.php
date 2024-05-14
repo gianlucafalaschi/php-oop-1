11b54b4c5c337246c5a6bccf417577cd27debc1e
gianlucafalaschi <gian100gian@outlook.it>
2024-05-14 15:36:51 +0200

    class and instances created

--- index.php
+++ index.php
@@ -17,4 +17,14 @@ $indipendeceDay->genre = 'Fantascienza';
 $indipendeceDay->year = '1996';
 
 var_dump($indipendeceDay);
+
+$interstellar = new Movie();
+$interstellar->title = 'Interstellar';
+$interstellar->director = 'Christopher Nolan';
+$interstellar->genre = 'Fantascienza';
+$interstellar->year = '2014';
+
+
+var_dump($interstellar);
+
 ?>
