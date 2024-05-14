32dd7fae6ae3209e9e92d4e1d59e8d1da19310fe
gianlucafalaschi <gian100gian@outlook.it>
2024-05-14 15:54:10 +0200

    construct function implemented

--- index.php
+++ index.php
@@ -7,24 +7,30 @@ class Movie {
     public $year;
 
 
+// COSTRUTTORE (viene chiamata con new)
+public function __construct($_title, $_genre) {
+    $this->title = $_title;
+    $this->genre = $_genre;
+}
+
 }
 
 
-$indipendeceDay = new Movie();
-$indipendeceDay->title = 'Indipendece Day';
+$indipendeceDay = new Movie('Indipendence Day', 'Fantascienza');
+/* $indipendeceDay->title = 'Indipendece Day'; */
 $indipendeceDay->director = 'Roland Emmerich';
-$indipendeceDay->genre = 'Fantascienza';
+/* $indipendeceDay->genre = 'Fantascienza'; */
 $indipendeceDay->year = '1996';
 
 var_dump($indipendeceDay);
 
-$interstellar = new Movie();
-$interstellar->title = 'Interstellar';
+$interstellar = new Movie('Interstellar', 'Fantascienza');
+/* $interstellar->title = 'Interstellar'; */
 $interstellar->director = 'Christopher Nolan';
-$interstellar->genre = 'Fantascienza';
+/* $interstellar->genre = 'Fantascienza'; */
 $interstellar->year = '2014';
 
 
-var_dump($interstellar);
+var_dump($interstellar); 
 
 ?>
